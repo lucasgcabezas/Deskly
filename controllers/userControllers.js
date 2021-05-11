93ef9f6c6a73e31224147fd844b07c28ba044d9c
Pablo Ceresola <pceresola@gmail.com>
2021-05-11 14:47:44 -0300

    retoque

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -74,3 +74,5 @@ const userControllers={
 }
 
 module.exports = userControllers
+
+
--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const userScheema = mongoose.Schema({
     lastName: {type: String}, required: true,
     email: {type: String, required: true},
     img: {type: String, required: true},
-    role: {type: String, required: true},
+    role: {type: String},
     password: {type: String, required: true},
     google:{type:Boolean, default:false},
   }) 
