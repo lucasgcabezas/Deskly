4faa6770d4aae5d849c094d3b8ac7f2c874a6d46
Pablo Ceresola <pceresola@gmail.com>
2021-05-11 14:55:01 -0300

    arreglo modelo

--- models/user.js
+++ models/user.js
@@ -5,7 +5,6 @@ const userScheema = mongoose.Schema({
     lastName: {type: String}, required: true,
     email: {type: String, required: true},
     img: {type: String, required: true},
-    role: {type: String},
     password: {type: String, required: true},
     google:{type:Boolean, default:false},
   }) 
