d3bf645bb2000efd4d1f141ba8fe60e3857ad82a
lucasgcbzs <77704551+lucasgcbzs@users.noreply.github.com>
2021-05-11 15:35:08 -0300

    merge corregido backend

--- models/user.js
+++ models/user.js
@@ -1,14 +1,14 @@
-const mongoose = require ("mongoose")
+const mongoose = require("mongoose")
 
-const userScheema = mongoose.Schema({
+const userSchema = new mongoose.Schema({
     firstName: {type: String, required: true},
-    lastName: {type: String}, required: true,
+    lastName: {type: String, required: true},
     email: {type: String, required: true},
     img: {type: String, required: true},
     password: {type: String, required: true},
-    google:{type:Boolean, default:false},
+    google:{type: Boolean, default:false},
   }) 
 
-const User = mongoose.model("user", userScheema)
+const User = mongoose.model("user", userSchema)
 
 module.exports = User
