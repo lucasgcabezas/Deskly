6dcfd453f4518297feefad7262fc5ae577a1d9bd
lucasgcbzs <77704551+lucasgcbzs@users.noreply.github.com>
2021-05-12 09:44:52 -0300

    volo schedule

---
+++ controllers/taskplannerControllers.js
@@ -0,0 +1,65 @@
+const Taskplanner = require('../models/TaskplannerModel')
+
+const taskplannerControllers = {
+    addTaskplanner: async(req,res) =>{
+        const newTaskplanner = new Taskplanner(req.body)
+        try{
+            await newTaskplanner.save()
+            res.json({success: true, respuesta: newTaskplanner})
+        }catch(error){
+            res.json({success: false, respuesta: 'An error has ocurred'})
+            console.log(error)
+        }
+    },
+    putTaskplanner: async(req, res) => {
+        try{
+            const taskplanner = await Taskplanner.findOneAndUpdate({_id: req.params.id}, {...req.body}, {new: true})
+            res.json({success: true, respuesta: taskplanner})
+        }catch(error){
+            res.json({success: false, respuesta: 'An error has occurred'})
+            console.log(error)
+        }
+    },
+
+    deleteTaskplanner: async (req, res) => {
+        try{
+            const taskplanner = await Taskplanner.findOneAndDelete({_id: req.params.id})
+            res.json({success: true, respuesta: taskplanner})
+        }catch(error){
+            res.json({success: false, respuesta: 'An error has occurred'})
+            console.log(error)
+        }
+    },
+    
+    getTaskplannerFromBoard: async(req,res) => {
+        try{
+            const taskplanner = await Taskplanner.find({boardId: req.params.id})
+            res.json({success: true, respuesta: taskplanner})
+        }catch(error){
+            res.json({success: false, respuesta: 'An error has occurred'})
+            console.log(error)
+        }
+    },
+
+    getAllTaskplanner: async (req,res) => {
+        try{
+            const taskplanner = await Taskplanner.find()
+            res.json({success: true, respuesta: taskplanner})
+        }catch(error){
+            res.json({success: false, respuesta: 'An error has occurred'})
+            console.log(error)
+        }
+    },
+
+    getTaskplanner: async (req, res) => {
+        try{
+            const taskplanner = await Taskplanner.find({_id: req.params.id})
+            res.json({success: true, respuesta: taskplanner})
+        }catch(error){
+            res.json({success: false, respuesta: 'An error has occurred'})
+            console.log(error)
+        }
+    }
+}
+
+module.exports = taskplannerControllers
--- models/TaskModel.js
+++ models/TaskModel.js
@@ -6,7 +6,7 @@ const taskSchema = new mongoose.Schema({
     priority: {type: Number, min:1, max:5},
     comments: [{userId:{type: mongoose.Types.ObjectId, ref:'user'}, userCompleteName: {type: String}, message:{type: String}}],
     verify: {type: Boolean, default: false},
-    scheduleId: {type:mongoose.Types.ObjectId, ref: 'schedule', required: true}
+    taskplannerId: {type:mongoose.Types.ObjectId, ref: 'taskplanner', required: true}
 })
 const TaskModel = mongoose.model('task', taskSchema)
 module.exports = TaskModel
