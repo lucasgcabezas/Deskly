f21a73f9f473af42aa4cba0e7137e2457d08cb93
lucasgcbzs <77704551+lucasgcbzs@users.noreply.github.com>
2021-05-11 21:43:35 -0300

    inicio de frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import Home from './pages/Home'
@@ -7,16 +7,16 @@ import Sign from './pages/Sign'
 const App = () => {
   return (
     <>
-    <Header/>
-    <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Home}/>
-      <Route path="/mydesk" component={MyDesk}/>
-      <Route path="/sign" component={Sign}/>
-      <Redirect to="/"/>
-    </Switch>
-    </BrowserRouter>
-    <Footer/>
+      <BrowserRouter>
+      <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/mydesk" component={MyDesk} />
+          <Route path="/sign" component={Sign} />
+          <Redirect to="/" />
+        </Switch>
+      <Footer />
+      </BrowserRouter>
     </>
   )
 
--- frontend/src/pages/Sign.js
+++ frontend/src/pages/Sign.js
@@ -1,5 +1,12 @@
+import LogIn from '../components/LogIn'
+
 const Sign = () => {
-    return ( <h1>Soy la page Sign</h1> );
+    return (
+        <>
+            <h1>Soy la page Sign</h1>
+            <LogIn />
+        </>
+    )
 }
- 
+
 export default Sign;
