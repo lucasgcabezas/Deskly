39f4b10166733297d92c3bf0eb8d026de75f9c2d
Adriel Baez <baez.adriel27@gmail.com>
2021-05-11 21:10:20 -0300

    agregando pagina Home, MyDesk y Sign

---
+++ frontend/src/pages/MyDesk.js
@@ -0,0 +1,5 @@
+const MyDesk = () => {
+    return ( <h1>soy myDesk</h1> );
+}
+ 
+export default MyDesk;
---
+++ frontend/src/pages/Sign.js
@@ -0,0 +1,5 @@
+const Sign = () => {
+    return ( <h1>Soy la page Sign</h1> );
+}
+ 
+export default Sign;
