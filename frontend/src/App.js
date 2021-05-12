d844a98fcc828dd1bedabe0cc07a7dd56b8187c2
diana sofia parra <sofiaparra.15@gmail.com>
2021-05-11 21:14:26 -0300

    componentes header footer nav, router en app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,25 @@
-
-function App() {
-  return <span>hola</span>
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Home from './pages/Home'
+import MyDesk from './pages/MyDesk'
+import Sign from './pages/Sign'
+const App = () => {
+  return (
+    <>
+    <Header/>
+    <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={Home}/>
+      <Route path="/mydesk" component={MyDesk}/>
+      <Route path="/sign" component={Sign}/>
+      <Redirect to="/"/>
+    </Switch>
+    </BrowserRouter>
+    <Footer/>
+    </>
+  )
 
 }
 
-export default App;
+export default App
