6153c7be616ac6ece6bbaa9e032d55dd4394da1c
Adriel Baez <baez.adriel27@gmail.com>
2021-05-12 14:23:08 -0300

    agregando componente taskModal y commentActions

---
+++ frontend/src/components/TaskModal.js
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import {connect} from 'react-redux';
+import commentActions from '../redux/actions/commentActions'
+
+const TaskModal = ({addComment}) => {
+    const [newComment, setNewComment] = useState('')
+    const [show, setShow] = useState(true)
+    let display = !show ? 'none' : 'block'
+
+    const sendComment = async () => {
+        if (newComment === '') {
+            alert('comentario vacio')
+            return false
+        }
+        let taskId= '609be485d3f81948bc8b48dd'
+        await addComment(taskId, newComment)
+        setNewComment('')
+    }
+    return (
+        <>
+            <button onClick={() => setShow(true)}>Show</button>
+            {show &&
+                <div style={{ display: display }}>
+                    <div>
+                        <div>
+                            <h3>titulo de la tarea</h3>
+                            <p>en la lista "nombre de la lista de tarea"</p>
+                        </div>
+                        <button onClick={() => setShow(false)}>X</button>
+                    </div>
+                    <div>
+                        <h3>Descripcion</h3>
+                        <div>
+                            <input placeholder="Añadir una descripcion"></input>
+                        </div>
+                    </div>
+                    <div>
+                        <h3>Actividad</h3>
+                        <div>
+                            <h1>comment del usuario</h1>
+                        </div>
+                        <div>
+                            <div>
+                                <p>foto user</p>
+                            </div>
+                            <div>
+                                <input placeholder="Escriba un comentario..." value={newComment} onChange={(e) => setNewComment(e.target.value)}></input>
+                                <button onClick={sendComment}>Guardar</button>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            }
+        </>
+
+    );
+}
+
+
+const mapDispatchToProps = {
+    addComment: commentActions.addComment
+}
+
+export default connect(null, mapDispatchToProps)(TaskModal)
---
+++ frontend/src/redux/actions/commentActions.js
@@ -0,0 +1,51 @@
+import axios from 'axios'
+
+
+const commentActions = {
+    
+    addComment: (taskId, newComment) => {
+        return async (dispatch, getState) => {
+            try {
+                    console.log(taskId,newComment);
+                const response = await axios.post('http://localhost:4000/api/task/comment/' + taskId, newComment)
+
+                // newComment : {objectId del usuario , nombre del usuario , mensaje}
+
+                return response.data.response.comments // Array de todos los comentarios con el nuevo
+
+            } catch(error) {
+                console.log('error en tasks actions ',error)
+            }
+        }
+    },
+
+    editComment: (taskId, editedComment) => {
+        return async (dispatch, getState) => {
+            try {
+                const response = await axios.put('http://localhost:4000/api/task/comment/' + taskId, editedComment)
+
+                // editedComment : {id fel comentario , mensaje}
+
+                return response.data.response.comments // Array de todos los comentarios con el editado
+
+            } catch {
+                console.log('error en tasks actions ')
+            }
+        }
+    },
+
+    deleteComment: (taskId) => {
+        return async (dispatch, getState) => {
+            try {
+                const response = await axios.delete('http://localhost:4000/api/task/comment/' + taskId)
+                
+                return response.data.response.comments // Array de todos los comentarios si el borrado
+
+            } catch {
+                console.log('error en tasks actions ')
+            }
+        }
+    }
+}
+
+export default commentActions
