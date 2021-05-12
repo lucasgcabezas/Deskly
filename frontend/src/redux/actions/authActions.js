580f68b0e515628c4e1cf3bdcd66dc5381f2bff4
lucasgcbzs <77704551+lucasgcbzs@users.noreply.github.com>
2021-05-11 21:11:44 -0300

    auth reducer y action

---
+++ frontend/src/redux/actions/authActions.js
@@ -0,0 +1,67 @@
+import axios from 'axios'
+
+const authActions = {
+    signUpUser: (user) => {
+        return async (dispatch, getState) => {
+
+            try {
+                const response = await axios.post('http://localhost:4000/api/newuser', user)
+                // if (response.data.errorsValidator) {
+                //     return response.data.errorsValidator
+
+                // } else if (response.data.error) {
+                //     alert('Error',response.data.error, 'danger')
+
+                // } else {
+                    dispatch({ type: 'LOG_USER', payload: response.data.response })
+                    alert(response.data.response.firstName,`Welcome to Mytinerary!`, 'success')
+                // }
+            } catch {
+                alert('Error','Internal server error, please try later!', 'danger')
+
+            }
+        }
+    },
+
+    signInUSer: (userToSignIn, googleFlag) => {
+        return async (dispatch, getState) => {
+            try {
+                const response = await axios.post('http://localhost:4000/api/login', userToSignIn, googleFlag )
+                // if (!response.data.success) {
+                //     alert('Oops',response.data.error, 'danger')
+                // } else {
+                    dispatch({ type: 'LOG_USER', payload: response.data.response })
+                    alert(response.data.response.firstName,`Welcome to Mytinerary!`, 'success')
+                // }
+            } catch {
+                alert('Error','Internal server error, please try later!', 'danger')
+            }
+        }
+    },
+
+    signInLocalStorage: (userLocalStorage) => {
+        return async (dispatch, getState) => {
+            try {
+                const response = await axios.get('http://localhost:4000/api/relogin', {
+                    headers: { 'Authorization': 'Bearer ' + userLocalStorage.token }
+                })
+                dispatch({ type: 'LOG_USER', payload: { ...response.data.response, token: userLocalStorage.token } })
+
+            } catch (err) {
+                if (err.response.status === 401) {
+                    alert("Me parece que me estás queriendo cagar con un token falso...")
+                }
+            }
+        }
+    },
+
+    signOut: () => {
+        return async (dispatch, getState) => {
+            alert('Goodbye!', 'Hope to see you soon!', 'info')
+            dispatch({ type: 'LOGOUT_USER' })
+
+        }
+    }
+}
+
+export default authActions
---
+++ frontend/src/redux/reducers/authReducer.js
@@ -0,0 +1,23 @@
+const initialState = {
+    userLogged: null,
+}
+
+const authReducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case 'LOG_USER':
+            localStorage.setItem('userLogged', JSON.stringify({ firstName: action.payload.firstName, userPic: action.payload.userPic }))
+            localStorage.setItem('token', action.payload.token)
+            return { ...state, userLogged: action.payload }
+
+        case 'LOGOUT_USER':
+            localStorage.clear()
+            return { ...state, userLogged: null }
+
+        default:
+            return state
+    }
+}
+
+export default authReducer
+
