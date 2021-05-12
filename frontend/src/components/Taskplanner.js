969487ae2987b119bb6c589b237e8420e463353a
lucasgcbzs <77704551+lucasgcbzs@users.noreply.github.com>
2021-05-12 20:42:16 -0300

    funciona bigotteee!!

--- frontend/src/components/Taskplanner.js
+++ frontend/src/components/Taskplanner.js
@@ -10,14 +10,18 @@ const TaskPlanner = (props) => {
     const [open, setOpen] = useState(false)
     const [newTitle, setNewTitle] = useState('')
     const [editTitle, setEditTitle] = useState(true)
-    console.log(props)
+    // console.log(props)
+
     useEffect(() => { fetchAllTasks() }, [])
 
     const fetchAllTasks = async () => {
-        const response = await props.tasksFromTaskplanner
+        const response = await props.tasksFromTaskplanner(props.taskplanner._id)
+        console.log(response)
         setAllTasks(response)
         setPreloader(false)
     }
+
+
 
     const enter = (e, condition) => {
         if (condition === 'task' && e.key === 'Enter') {
@@ -36,28 +40,31 @@ const TaskPlanner = (props) => {
         }
     }
 
-    
-    return(
-        <div>
+
+    // console.log(props.taskplanner._id)
+
+    return (
+        <div >
             <div>
-                <button onClick={props.erase(props.taskplanner._id)}>Delete</button>
+                <button onClick={() => props.erase(props.taskplanner._id)}>Delete</button>
             </div>
-            {editTitle && <h1 onClick={setEditTitle(!editTitle)}>{props.taskplanner.title}</h1>}
+            {editTitle && <h1 onClick={() => setEditTitle(!editTitle)}>{props.taskplanner.title}</h1>}
             {!editTitle && <div>
-                <input onKeyDown={(e)=>enter(e,'edit')} type="text" value={newTitle} onChange={(e) => setNewTitle(e.target.value)}/>
-                <button onClick={newTitle.trim() && props.edit(props.taskplanner._id, newTitle)}>Send</button>
-            </div> 
+                <input onKeyDown={(e) => enter(e, 'edit')} type="text" value={newTitle} onChange={(e) => setNewTitle(e.target.value)} />
+                <button onClick={newTitle.trim() && (() => props.edit(props.taskplanner._id, newTitle))}>Send</button>
+            </div>
             }
-            <button onClick={setOpen(!open)}></button>
+            <button onClick={()=>setOpen(!open)}></button>
             {
                 open && <div>
-                    <input onKeyDown={(e)=>enter(e,'task')} type="text" value={newTitle} onChange={(e) => setNewTitle(e.target.value)}/>
+                    <input onKeyDown={(e) => enter(e, 'task')} type="text" value={newTitle} onChange={(e) => setNewTitle(e.target.value)} />
                     <button onClick={sendValues}>Send</button>
                 </div>
             }
 
             <div>
-                {preloader
+                {
+                preloader 
                     ? <span>cargando</span>
                     : allTasks.map(task => <Task key={task._id} task={task} allTasks={allTasks} setAllTasks={setAllTasks} />)
                 }
